63056a1973eef59f0a45d3b979ebf3ea4462b060
abc <abc>
2023-11-08 10:26:56 +0800

    背包图片

--- scripts/prefabs/mybackpack.lua
+++ scripts/prefabs/mybackpack.lua
@@ -1,5 +1,6 @@
 local assets=
 {
+    Asset("ATLAS", "images/inventoryimages/mybackpack.xml")
 }
 
 local function onequip(inst, owner) 
@@ -52,8 +53,8 @@ local function fn(Sim)
     inst:AddComponent("inspectable")
     
     inst:AddComponent("inventoryitem")
-    inst.components.inventoryitem.cangoincontainer = false
     inst.components.inventoryitem.foleysound = "dontstarve/movement/foley/backpack"
+    inst.components.inventoryitem.atlasname = "images/inventoryimages/mybackpack.xml" -- 设置物品栏图片文档。官方内置的物体有默认的图片文档，所以不需要设置这一项，但自己额外添加的物体使用自己的图片文档，就应该设置这一项
 
     MakeInventoryFloatable(inst, "idle_water", "anim")
 
